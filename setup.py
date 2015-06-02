b10f0941149467f7ce50c9d38f1760f36508f8cc
VolosSaif <saif.sultan@volossoftware.com>
2015-06-02 23:48:34 +0000

    setup added

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='A sample Python project by saif',
     long_description='here is my long description',
