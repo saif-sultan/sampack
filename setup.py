8053d319950910de7a7fcb4f409f43a6cb2873ca
VolosSaif <saif.sultan@volossoftware.com>
2015-06-02 23:44:31 +0000

    setup added

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=saif:main',
+            'hahasamer=saif:main',
         ],
     },
 )
