56da20ed4edda28f442aca1538575c91883f76c6
VolosSaif <saif.sultan@volossoftware.com>
2015-06-03 00:10:50 +0000

    package added mypack

--- setup.py
+++ setup.py
@@ -69,8 +69,7 @@ setup(
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-    package_dir = {'': 'mypackage'},
-
+    install_requires=['APA'],
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
