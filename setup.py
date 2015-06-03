bc5fd2674a76225d34073193564ea3d21ef8dfdb
VolosSaif <saif.sultan@volossoftware.com>
2015-06-03 00:04:42 +0000

    package added mypack

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
+    package_dir = {'': 'safa'},
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
